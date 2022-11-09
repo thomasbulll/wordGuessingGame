29e79363892a3e47e566aace262a59c0fe0f9b16
Thomas Bull <thomasbull@MASTUDENT-Thomas-Bull-THFD966NGP.local>
2022-11-09 12:58:27 +0000

    added additional words to guess from

--- app/src/main/java/wordguessinggame/WordChoser.java
+++ app/src/main/java/wordguessinggame/WordChoser.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 public class WordChoser {
 
-    public static final String[] DICTIONARY = {"MAKERS", "CANDIES", "DEVELOPER", "LONDON"};
+    public static final String[] DICTIONARY = {"MAKERS", "CANDIES", "DEVELOPER", "LONDON", "YELLOW", "VISION", "COMBAT", "CODING", "SOFTWARE",
+     "BUILDING", "VENDOR", "UNIQUE", "THEORY", "ROBOTIC", "POLICE", "OXFORD", "GOOGLE", "FAMOUS"};
     
     public static void main(String[] args){
 
